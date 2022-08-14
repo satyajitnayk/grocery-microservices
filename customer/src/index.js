86560a44950263adb02e1ac9e552e162b9fc5806
satyajit nayak <satyajitn15@gmail.com>
2022-08-15 00:19:37 +0530

    Refactor customer microservice.

---
+++ customer/src/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const { PORT } = require('./config');
+const { databaseConnection } = require('./database');
+const expressApp = require('./express-app');
+require('dotenv').config();
+
+const StartServer = async () => {
+  // console.log(process.env.MONGODB_URI);
+  const app = express();
+
+  await databaseConnection();
+
+  await expressApp(app);
+
+  app
+    .listen(PORT, () => {
+      console.log(`listening to port ${PORT}`);
+    })
+    .on('error', (err) => {
+      console.log(err);
+      process.exit();
+    });
+};
+
+StartServer();
